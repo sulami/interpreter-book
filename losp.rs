ef363d2c55b2196a9fe4bbc036fa69976653e4a0
Robin Schroer <sulami@peerwire.org>
2019-07-28 11:24:33 +0200

    use methods

--- losp.rs
+++ losp.rs
@@ -6,7 +6,7 @@ enum OpCode {
 }
 
 enum Value {
-    Float(f64)
+    Float(f64),
 }
 
 impl std::fmt::Display for Value {
@@ -25,23 +25,29 @@ struct Chunk {
     constants: ValueArray,
 }
 
+impl Chunk {
+    fn read_constant(&self, index: usize) -> &Value {
+        &self.constants[index]
+    }
+
+    fn disassemble(&self, name: &str) {
+        println!("== {} ==", name);
+        for i in 0..self.code.len() {
+            disassemble_instruction(self, i)
+        }
+    }
+}
+
 fn disassemble_instruction(chunk: &Chunk, offset: usize) {
     let instruction: &OpCode = &chunk.code[offset];
-    if offset > 0  && &chunk.lines[offset] == &chunk.lines[offset-1] {
+    if offset > 0 && &chunk.lines[offset] == &chunk.lines[offset-1] {
         print!("{:04x} {:>5} ", offset, "|");
     } else {
         print!("{:04x} {:>5} ", offset, &chunk.lines[offset]);
     };
     match instruction {
-        OpCode::Constant(ptr) => println!("CONSTANT \t{} \t{}", ptr, chunk.constants[*ptr]),
+        OpCode::Constant(ptr) => println!("CONSTANT \t{} \t{}", ptr, chunk.read_constant(*ptr)),
         OpCode::Return => println!("RETURN"),
-    }
-}
-
-fn disassemble_chunk(chunk: &Chunk, name: &str) {
-    println!("== {} ==", name);
-    for i in 0..chunk.code.len() {
-        disassemble_instruction(chunk, i)
     }
 }
 
@@ -53,5 +59,5 @@ fn main() {
         lines: lines,
         constants: constant_pool,
     };
-    disassemble_chunk(&chunk, "test chunk");
+    chunk.disassemble("test chunk");
 }
